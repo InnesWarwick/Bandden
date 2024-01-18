8e7d9fbf00d7b9b5eb7c9921756c36a7d2c767b3
InnesWarwick <inneswarwick@gmail.com>
2024-01-18 14:53:31 +0000

    renamed:    advert.html -> advert.php 	renamed:    browse.html -> browse.php 	renamed:    contactUs.html -> contactUs.php 	deleted:    index.html 	new file:   index.php     renamed:    profile.html -> profile.php 	deleted:    style.css 	new file:   stylesheets/advertStyle.css 	new file:   stylesheets/browseStyle.css 	new file:   stylesheets/contactStyle.css     new file:   stylesheets/generalStyle.css 	new file:   stylesheets/indexStyle.css 	new file:   stylesheets/profileStyle.css

---
+++ profile.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheets/generalStyle.css">
+    <link rel="stylesheet" href="stylesheets/profileStyle.css">
+    <title>Bandden</title>
+</head>
+
+<body>
+    <div class="logoDiv">
+        <img src="BanndenLogo.png" alt="website logo">
+        <a href="profile.php">
+        <img src="icons/account_circle_FILL0_wght400_GRAD-25_opsz40.png" alt="profile photo" class="profileIcon">
+        </a>
+    </div>
+    <div class="navBar">
+        <a class="homeButton" href="index.php">Home</a>
+        <a class="browseButton" href="browse.php">Browse</a>
+        <a class="contactUs" href="contactUs.php"> contact us</a>
+    </div>
+    <main class="contentBlock">
+        <div class="profileInfo">
+            <h2>Username</h2>
+            <h3><a>Guitar</a>
+                <a>Drums</a>
+                <a>production</a>
+            </h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel dapibus massa. Cras vitae fermentum enim.
+                Vivamus viverra ligula eu maximus cursus. Nam dui lacus, rhoncus a tempus quis, mattis quis elit. Nulla
+                vel condimentum ex. Pellentesque tincidunt fermentum sem, at volutpat lectus bibendum ut. Aliquam
+                laoreet lacus quam, sit amet sodales eros venenatis at. Duis bibendum dictum turpis, nec imperdiet nulla
+                dapibus eu. Ut ullamcorper ullamcorper auctor. Nunc dictum orci elit, non ultricies velit bibendum et.
+                Vivamus nec arcu et ipsum rutrum ullamcorper. Phasellus at lorem eu eros convallis ullamcorper eu at ex.
+                Phasellus pulvinar elementum volutpat.</p>
+        </div>
+    </main>
+    <div class="footerBar"></div>
+</body>
+</html>
